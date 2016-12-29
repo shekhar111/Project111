2f5fb4812e50ef6a24ff18862b76d640d2a864e4
shekhar <shekhar@shashi>
2016-12-29 21:52:48 +0530

    Update 2

---
+++ JenkinsDemo/runTestNG.bat
@@ -0,0 +1,5 @@
+d:
+cd D:\Workspace-neon\SeleniumWorkspace\JenkinsDemo
+set classpath=D:\Workspace-neon\SeleniumWorkspace\JenkinsDemo\bin;D:\Workspace-neon\SeleniumWorkspace\JenkinsDemo\lib\*
+java org.testng.TestNG testng.xml
+pause
