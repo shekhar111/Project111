6397b36fceec11bbbf5cb3b1363284d9e764232f
shekhar111 <shekhar.shashi111@gmail.com>
2016-12-29 21:42:56 +0530

    Updated 1

---
+++ JenkinsDemo/src/demoJenkins/DemoJenkinsJobs.java
@@ -0,0 +1,13 @@
+package demoJenkins;
+
+import org.testng.annotations.Test;
+
+public class DemoJenkinsJobs {
+	
+	@Test
+	public void testJenkins()
+	{
+		System.out.println("Welcome to the world of jenkins!!!");
+	}
+
+}
